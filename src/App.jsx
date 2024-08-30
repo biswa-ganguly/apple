57b96686070e46e5abc03401500538cd59f365a8
biswa-ganguly <biswaganguly16@gmail.com>
2024-08-31 00:00:26 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Box } from '@react-three/drei';
+import React from 'react';
+import { OrbitControls, Environment, ScrollControls } from '@react-three/drei';
 import './style.css';
+import MacCointainer from './MacCointainer';
 
 function App() {
   return (
-    <Canvas>
-      <OrbitControls />
-      <ambientLight intensity={0.5} />
-      <pointLight position={[10, 10, 10]} />
-      <Box args={[1, 1, 1]}>
-        <meshStandardMaterial attach="material" color="royalblue" />
-      </Box>
+    <div className="w-full h-screen font-sf-pro">
+      <div className='absolute top-28 left-1/2 -translate-x-1/2'>
+        <h2 className='text-white text-7xl tracking-tighter font-[700]'>macbook pro.</h2>
+      </div>
+    <Canvas camera={{ fov: 12, position: [0, -10, 220] }}>
+      {/* <OrbitControls /> */}
+      <Environment files={["https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/4k/studio_small_09_4k.exr"]} />
+
+      <ScrollControls pages={3}>
+      <MacCointainer />
+      </ScrollControls>
     </Canvas>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'helvetica-now': ['"Helvetica_Now_Display"', 'sans-serif']
+      }
+    }
   },
   plugins: [],
 }
